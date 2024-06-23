fea0088e72bbd98a02210f310c28aa4e23bd54d0
Shashank Singh <700singhshashank@.com>
2024-06-23 13:15:32 +0530

    connect to mongo Atlas

---
+++ src/index.js
@@ -0,0 +1,47 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv" // importing all env is essential as soon as possible 
+import connectDB from "./db/indexdb.js";
+
+// importing all the env
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Approach 1 writting the connection code in index.js using the iife
+/*
+import express from'express';
+const app=express()
+
+;( async() => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on('error',()=>{
+            console.log("ERRR: ",error);
+            throw error
+        })
+        
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.error("Error",error)
+        throw error
+    }
+})()
+*/

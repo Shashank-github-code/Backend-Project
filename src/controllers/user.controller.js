fa80f9e2174bda6fb990dd925e5056646e2ec25a
Shashank Singh <700singhshashank@.com>
2024-07-05 13:11:34 +0530

    add register and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,85 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/apiError.js"
+import {User} from "../models/user.model.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import {ApiResponse} from "../utils/apiResponse.js"
 
 const registerUser = asyncHandler( async (req,res)=>{
-    return res.status(200).json({
-        message:"OK"
+
+    // get user details from frontend through (Postman)
+    // validation - not empty
+    // check if user already exits : username, email
+    // check for images, check for avatar
+    // upload them to cloudinary. avatar 
+    // create user object - create emtry in DB
+    // remove password and refresh token field from reponse
+    // check for user creation 
+    // return response
+
+    const {fullName, email, username, password} = req.body  // all the data comes from the body portion of frontend and this is destructured
+    console.log("email :", email);
+
+    // if(fullName === ""){
+    //     throw new ApiError(400, "Full name is required");
+    // }
+
+    //instead of writting if for all fields we can use .some method
+    if (
+        [fullName, email, username, password].some((field)=>
+        field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All fields are compulsory")
+    }
+
+    // here User is mongoose created(in user model) that talks to DB
+    // .findOne is a database call
+    const existedUser = User.findOne({   //$or takes an array of object to apply any function like .findOne so if any field matches either of the two 
+        $or:[{ username }, { email }]
     })
+
+    if(existedUser){
+        throw new ApiError(409, "User with email or username already exist")
+    }
+
+    //uploading files on cloudinary
+    // req.files we get from multer
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is required");
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!avatar){
+        throw new ApiError(400, "Avatar file is required");
+    }
+
+    // sending in database
+    const user = await User.create({
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+    })
+    //checking if user created or not
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"// these two fields are not selected
+    )
+
+    if(!createdUser){
+        throw new ApiError(500, "Something went wrong while registering the user")
+    }
+
+    // after creating returning the response
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered successfully")
+    )
+
 })
 
 export { registerUser, }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,27 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middleware/multer.middleware.js"
 
 //This line creates a new instance of a router object. This router object will be used to define the routes.
 const router = Router()
 
 //router.route("/register"): This specifies that the route being defined is /register.
 // .post(registerUser): This specifies that the route /register will respond to HTTP POST requests. When a POST request is made to /register, the registerUser function will be called to handle the request. The registerUser function is expected to contain the logic for handling user registration.
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    // using a middleware just before the registerUser method
+    //.fields accepts array
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+)
 
 
 export default router

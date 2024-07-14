84c1b1647ddd57c776beb84bbb4efb2d7de5eb2c
Shashank Singh <700singhshashank@gmail.com>
2024-07-14 17:24:23 +0530

    checking errors

--- src/db/indexdb.js
+++ src/db/indexdb.js
@@ -4,8 +4,8 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`, {
-            serverSelectionTimeoutMS: 30000, // Increased timeout to 30 seconds
-            connectTimeoutMS: 30000, // Connection timeout set to 10 seconds
+            // serverSelectionTimeoutMS: 30000, // Increased timeout to 30 seconds
+            // connectTimeoutMS: 30000, // Connection timeout set to 10 seconds
         });
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {

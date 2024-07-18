a1453214f86f2fb2120198ddf751d983a0bd962d
Shashank Singh <700singhshashank@gmail.com>
2024-07-18 11:39:32 +0530

    Completed the video controllers

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { Playlist } from "../models/playlist.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { Video } from "../models/video.model.js";
 import { User } from "../models/user.model.js";
 
 const createPlaylist = asyncHandler(async (req, res) => {
@@ -53,6 +54,100 @@ const getUserPlaylists = asyncHandler(async (req, res) => {
     return res.status(200).json(new ApiResponse(200, "User playlists retrieved successfully", playlists));
 });
 
+const addVideo = asyncHandler(async (req, res) => {
+    const {playlistId, videoId} = req.params;
+    
+    // validate the ids
+    if(!mongoose.Types.ObjectId.isValid(videoId) || !mongoose.Types.ObjectId.isValid(playlistId)){
+        throw new ApiError(400, "Invalid playlist or video ID");
+    }
 
+    // check if the ids are present or not
+    const playlist = await Playlist.findById(playlistId);// playlist get in this variable if present
+    if(!playlist){
+        throw new ApiError(400, "Playlist not found");
+    }
+    const video = await Video.findById(videoId);// similarly video get in this variable if present
+    if(!video){
+        throw new ApiError(400, "Video not found");
+    }
 
-export { createPlaylist, getUserPlaylists};
+    // check if video already exists in playlist
+    // playlist.videos is an array containing the IDs of videos (so can apply array oprations like .includes .indexOf)
+    const videoPresent =await playlist.videos.includes(videoId);
+    if(videoPresent){
+        throw new ApiError(400, "Video already exists in this playlist");
+    }
+
+    // Add the video to the playlist
+    playlist.videos.push(videoId);
+    await playlist.save();
+
+    return res.status(200).json(new ApiResponse(200, playlist, "Video added to playlist successfully"))
+});
+
+const removeVideo = asyncHandler(async (req, res) => {
+    const { playlistId, videoId} = req.params;
+
+    // validate the ids
+    if(!mongoose.Types.ObjectId.isValid(videoId) || !mongoose.Types.ObjectId.isValid(playlistId)){
+        throw new ApiError(400, "Invalid playlist or video ID");
+    }
+
+    // check if the ids are present or not
+    const playlist = await Playlist.findById(playlistId);// playlist get in this variable if present
+    if(!playlist){
+        throw new ApiError(400, "Playlist not found");
+    }
+    const video = await Video.findById(videoId);// similarly video get in this variable if present
+    if(!video){
+        throw new ApiError(400, "Video not found");
+    }
+
+    // check if video exists in playlist so to delete it
+    // playlist.videos is an array containing the IDs of videos in the playlist.
+    const videoIndex = await playlist.videos.indexOf(videoId);
+    if( videoIndex === -1){
+        return res.status(400).json(new ApiResponse(400, null, "Video is not in the playlist"));
+    }
+    playlist.videos.splice(videoIndex,1);//playlist.videos.splice(videoIndex, 1) removes 1 element starting from the position videoIndex.
+    await playlist.save();
+
+    return res.status(200).json(new ApiResponse(200, playlist, "video deleted successfully"))
+})
+
+const deletePlaylist = asyncHandler(async (req,res) => {
+    const {playlistId} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(playlistId)){
+        throw new ApiError(400, "Invalid playlist ID");
+    }
+    const playlist = await Playlist.findById(playlistId);
+    if(!playlist){
+        throw new ApiError(400, "Playlist not found");
+    }
+
+    await playlist.deleteOne()
+
+    return res.status(200).json(new ApiResponse(200, "Playlist deleted successfully"))
+})
+
+const updatePlaylist = asyncHandler(async (req,res) => {
+    const { playlistId} = req.params
+    const { name, description } = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(playlistId)){
+        throw new ApiError(400, "Invalid playlist ID");
+    }
+    const playlist = await Playlist.findById(playlistId);
+    if(!playlist){
+        throw new ApiError(400, "Playlist not found");
+    }
+    playlist.name = name;
+    playlist.description = description;
+    await playlist.save();
+
+    return res.status(200).json(new ApiResponse(200, "Playlist updated successfully"))
+})
+
+export { createPlaylist, getUserPlaylists, addVideo, removeVideo, deletePlaylist, updatePlaylist};
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createPlaylist, getUserPlaylists} from "../controllers/playlist.controller.js";
+import { createPlaylist, getUserPlaylists, addVideo, removeVideo, deletePlaylist, updatePlaylist} from "../controllers/playlist.controller.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -7,7 +7,10 @@ const router = express.Router();
 router.post("/create", verifyJWT, createPlaylist);
 // Route to get playlists of a specific user
 router.get('/:userId/playlists', verifyJWT, getUserPlaylists);
-
+router.route('/:playlistId/videos/:videoId/add').post(verifyJWT, addVideo);
+router.route('/:playlistId/videos/:videoId/delete').delete(verifyJWT, removeVideo);
+router.route('/:playlistId/delete').delete(verifyJWT, deletePlaylist);
+router.route('/:playlistId/update').patch(verifyJWT, updatePlaylist);
 
 
 export default router;
